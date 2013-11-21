8b97eae16bb2e900636cf2fd52539a98d275659b
Ricardo <acq11ra@sheffield.ac.uk>
2013-11-21 13:43:51 +0000

    Examples work with new likelihoods

--- GPy/examples/classification.py
+++ GPy/examples/classification.py
@@ -15,6 +15,12 @@ def oil(num_inducing=50, max_iters=100, kernel=None):
     """
     Run a Gaussian process classification on the three phase oil data. The demonstration calls the basic GP classification model and uses EP to approximate the likelihood.
 
+    :param num_inducing: number of inducing inputs
+    :type num_inducing: integer
+    :param max_iters: maximum number of iterations to carry during optimization
+    :type max_inters: integer
+    :kernel: kernel function to use
+    :type kernel: a GPy kernel
     """
     data = GPy.util.datasets.oil()
     X = data['X']
@@ -26,6 +32,7 @@ def oil(num_inducing=50, max_iters=100, kernel=None):
 
     # Create GP model
     m = GPy.models.SparseGPClassification(X, Y,kernel=kernel,num_inducing=num_inducing)
+    print 'Approximation used: EP'
 
     # Contrain all parameters to be positive
     m.tie_params('.*len')
@@ -41,13 +48,14 @@ def oil(num_inducing=50, max_iters=100, kernel=None):
     GPy.util.classification.conf_matrix(probs,Ytest)
     return m
 
-def toy_linear_1d_classification(seed=default_seed):
+def toy_linear_1d_classification(approximation='EP',seed=default_seed):
     """
     Simple 1D classification example using EP approximation
 
+    :param approximation: approximation algorithm to use
+    :type param: ('EP'|'Laplace')
     :param seed: seed value for data generation (default is 4).
     :type seed: int
-
     """
 
     data = GPy.util.datasets.toy_linear_1d_classification(seed=seed)
@@ -55,7 +63,11 @@ def toy_linear_1d_classification(seed=default_seed):
     Y[Y.flatten() == -1] = 0
 
     # Model definition
-    m = GPy.models.GPClassification(data['X'], Y)
+    if approximation not in ('EP','Laplace'):
+        raise NotImplementedError, 'Approximation not valid.'
+    else:
+        print 'Approximation used: %s' %approximation
+    m = GPy.models.GPClassification(data['X'], Y,approximation = approximation)
 
     # Optimize
     #m.update_likelihood_approximation()
@@ -72,48 +84,14 @@ def toy_linear_1d_classification(seed=default_seed):
 
     return m
 
-def toy_linear_1d_classification_laplace(seed=default_seed):
-    """
-    Simple 1D classification example using Laplace approximation
-
-    :param seed: seed value for data generation (default is 4).
-    :type seed: int
-
-    """
-
-    data = GPy.util.datasets.toy_linear_1d_classification(seed=seed)
-    Y = data['Y'][:, 0:1]
-    Y[Y.flatten() == -1] = 0
-
-    bern_noise_model = GPy.likelihoods.bernoulli()
-    laplace_likelihood = GPy.likelihoods.Laplace(Y.copy(), bern_noise_model)
-
-    # Model definition
-    m = GPy.models.GPClassification(data['X'], Y, likelihood=laplace_likelihood)
-
-    print m
-    # Optimize
-    #m.update_likelihood_approximation()
-    # Parameters optimization:
-    m.optimize('bfgs', messages=1)
-    #m.pseudo_EM()
-
-    # Plot
-    fig, axes = pb.subplots(2,1)
-    m.plot_f(ax=axes[0])
-    m.plot(ax=axes[1])
-    print(m)
-
-    return m
-
-
 def sparse_toy_linear_1d_classification(num_inducing=10,seed=default_seed):
     """
     Sparse 1D classification example
 
+    :param num_inducing: number of inducing inputs
+    :type num_inducing: integer
     :param seed: seed value for data generation (default is 4).
     :type seed: int
-
     """
 
     data = GPy.util.datasets.toy_linear_1d_classification(seed=seed)
@@ -123,6 +101,7 @@ def sparse_toy_linear_1d_classification(num_inducing=10,seed=default_seed):
     # Model definition
     m = GPy.models.SparseGPClassification(data['X'], Y,num_inducing=num_inducing)
     m['.*len']= 4.
+    print 'Approximation used: EP'
 
     # Optimize
     #m.update_likelihood_approximation()
@@ -152,9 +131,10 @@ def toy_heaviside(seed=default_seed):
     Y[Y.flatten() == -1] = 0
 
     # Model definition
-    noise_model = GPy.likelihoods.bernoulli(GPy.likelihoods.noise_models.gp_transformations.Heaviside())
-    likelihood = GPy.likelihoods.EP(Y,noise_model)
+    transformation = GPy.likelihoods.noise_models.gp_transformations.Heaviside()
+    likelihood = GPy.likelihoods.likelihood_constructors.bernoulli(Y,gp_link=transformation)
     m = GPy.models.GPClassification(data['X'], likelihood=likelihood)
+    print 'Approximation used: EP'
 
     # Optimize
     m.update_likelihood_approximation()
@@ -174,7 +154,8 @@ def crescent_data(model_type='Full', num_inducing=10, seed=default_seed, kernel=
     """
     Run a Gaussian process classification on the crescent data. The demonstration calls the basic GP classification model and uses EP to approximate the likelihood.
 
-    :param model_type: type of model to fit ['Full', 'FITC', 'DTC'].
+    :param model_type: type of approximation, within EP algorithms, to use.
+    :type model_type: ('Full'|'FITC'|'DTC')
     :param inducing: number of inducing variables (only used for 'FITC' or 'DTC').
     :type inducing: int
     :param seed: seed value for data generation.
@@ -196,6 +177,7 @@ def crescent_data(model_type='Full', num_inducing=10, seed=default_seed, kernel=
     elif model_type == 'FITC':
         m = GPy.models.FITCClassification(data['X'], Y, kernel=kernel, num_inducing=num_inducing)
         m['.*len'] = 3.
+    print 'Approximation used: EP'
 
     m.pseudo_EM()
     print(m)
