4f48c48635421c12e32049faa5a1401b4129cd9b
Ricardo <acq11ra@sheffield.ac.uk>
2013-11-21 14:02:11 +0000

    General class that handles EP, Laplace and exact Gaussian likelihood

--- GPy/likelihoods/likelihood.py
+++ GPy/likelihoods/likelihood.py
@@ -2,12 +2,13 @@ import numpy as np
 import copy
 from ..core.parameterized import Parameterized
 
-class likelihood(Parameterized):
+class Likelihood(Parameterized):
     """
     The atom for a likelihood class
 
-    This object interfaces the GP and the data. The most basic likelihood
-    (Gaussian) inherits directly from this, as does the EP algorithm
+    This object interfaces the GP and the data.
+    The most basic likelihood (Gaussian) inherits directly from this,
+    as does the EP and Laplace approximations.
 
     Some things must be defined for this to work properly:
 
@@ -21,18 +22,44 @@ class likelihood(Parameterized):
         - self.V : self.precision * self.Y
 
     """
-    def __init__(self):
+    def __init__(self,data,noise_model,normalize,offset=None,scale=None):
         Parameterized.__init__(self)
+
+        self.noise_model = noise_model
+        self.data = data
+        self.num_data, self.output_dim = self.data.shape
+        self.num_params = self._get_params().size
+        self.Z = 0. # a correction factor which accounts for the approximation made
         self.dZ_dK = 0
 
+        #Data transformation (aka normalization)
+        if normalize:
+            self._offset = data.mean(0)[None, :] if offset == None else offset
+            self._scale = data.std(0)[None, :] if scale == None else scale
+            # Don't scale outputs which have zero variance to zero.
+            for pj in np.nonzero(self._scale == 0.)[0]:
+                self._scale[pj] = 1.0e-3
+            #self._scale[np.nonzero(self._scale == 0.)] = 1.0e-3
+        else:
+            self._offset = np.zeros((1, self.output_dim))
+            self._scale = np.ones((1, self.output_dim))
+
+        self.check_data()
+
+    def check_data(self):
+        """
+        Checks if data values are appropiate for the noise model
+        """
+        self.noise_model.check_data(self.data)
+
     def _get_params(self):
-        raise NotImplementedError
+        return np.asarray(self.noise_model._get_params())
 
     def _get_param_names(self):
-        raise NotImplementedError
+        return self.noise_model._get_param_names()
 
-    def _set_params(self, x):
-        raise NotImplementedError
+    def _set_params(self, p):
+        return self.noise_model._set_params(p)
 
     def fit_full(self, K):
         """
@@ -47,10 +74,18 @@ class likelihood(Parameterized):
         pass
 
     def _gradients(self, partial):
-        raise NotImplementedError
+        return self.noise_model._gradients(partial)
 
-    def predictive_values(self, mu, var):
-        raise NotImplementedError
+    def predictive_values(self, mu, var, full_cov, **noise_args):
+
+        mean,_var,q1,q3 = self.noise_model.predictive_values(mu,var,full_cov,**noise_args)
+
+        #Un-normalize data
+        mean = mean * self._scale + self._offset
+        q1 = q1 * self._scale + self._offset
+        q3 = q3 * self._scale + self._offset
+        var = var * self._scale**2
+        return mean,var,q1,q3
 
     def log_predictive_density(self, y_test, mu_star, var_star):
         """
@@ -66,4 +101,5 @@ class likelihood(Parameterized):
         :param var_star: predictive variance of gaussian p(f_{*}|mu_{*}, var_{*})
         :type var_star: (Nx1) array
         """
-        raise NotImplementedError
+        y_rescaled = (y_test - self._offset)/self._scale
+        return self.noise_model.log_predictive_density(y_rescaled,mu_star,var_star)
