2d0649e9ed7d16037f82bc6fcfab9b22fed0adb2
Ricardo <acq11ra@sheffield.ac.uk>
2013-11-21 14:08:23 +0000

    minor changes

--- GPy/likelihoods/__init__.py
+++ GPy/likelihoods/__init__.py
@@ -1,7 +1,8 @@
+from exact import Exact
 from ep import EP
 from laplace import Laplace
 from ep_mixed_noise import EP_Mixed_Noise
-from gaussian import Gaussian
+from gaussian import Gaussian #TODO erase
 from gaussian_mixed_noise import Gaussian_Mixed_Noise
 import noise_models
-from noise_model_constructors import *
+from likelihood_constructors import *
